99541e4aefb100c701c0a040104d622a398fa766
Mamidisetty <127787361+Mamidisetty@users.noreply.github.com>
2025-03-18 22:31:48 +0530

    Added ProductRepositoryTest class to the domain

---
+++ catlog-service/src/test/java/com/suresh/bookstore/catlog/domain/ProductRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.suresh.bookstore.catlog.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest(
+        properties = {
+            "spring.test.database.replace=none",
+            "spring.datasource.url=jdbc:tc:postgresql:latest:///db",
+        })
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void shouldGetAllProducts() {
+        List<ProductEntity> products = productRepository.findAll();
+        assertThat(products).hasSize(14);
+    }
+}
