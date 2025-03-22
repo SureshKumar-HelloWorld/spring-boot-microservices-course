6a53c0ca1b3c7ecea7022399bc000ffa658776fc
Mamidisetty <127787361+Mamidisetty@users.noreply.github.com>
2025-03-22 23:25:19 +0530

    Modified unit test using ProductionControllerTest

--- catlog-service/src/test/java/com/suresh/bookstore/catlog/web/controllers/ProductControllerTest.java
+++ catlog-service/src/test/java/com/suresh/bookstore/catlog/web/controllers/ProductControllerTest.java
@@ -8,10 +8,9 @@ import static org.hamcrest.Matchers.is;
 import com.suresh.bookstore.catlog.AbstractIT;
 import com.suresh.bookstore.catlog.domain.Product;
 import io.restassured.http.ContentType;
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.jdbc.Sql;
-
-import java.math.BigDecimal;
 
 @Sql("/test-data.sql")
 class ProductControllerTest extends AbstractIT {
@@ -34,10 +33,10 @@ class ProductControllerTest extends AbstractIT {
     }
 
     @Test
-    void shouldGetProductByCode(){
+    void shouldGetProductByCode() {
         Product product = given().contentType(ContentType.JSON)
                 .when()
-                .get("/api/products/{code}","P100")
+                .get("/api/products/{code}", "P100")
                 .then()
                 .statusCode(200)
                 .assertThat()
@@ -47,22 +46,22 @@ class ProductControllerTest extends AbstractIT {
 
         assertThat(product.code()).isEqualTo("P100");
         assertThat(product.name()).isEqualTo("Confabulations");
-        assertThat(product.description()).isEqualTo("'Language is a body, a living creature ... and this creature's home is the inarticulate as well as the articulate'. John Berger's work has revolutionized the way we understand visual language. In this new book he writes about language itself, and how it relates to thought, art, song, storytelling and political discourse today. Also containing Berger's own drawings, notes, memories and reflections on everything from Albert Camus to global capitalism, Confabulations takes us to what is 'true, essential and urgent'.");
+        assertThat(product.description())
+                .isEqualTo(
+                        "'Language is a body, a living creature ... and this creature's home is the inarticulate as well as the articulate'. John Berger's work has revolutionized the way we understand visual language. In this new book he writes about language itself, and how it relates to thought, art, song, storytelling and political discourse today. Also containing Berger's own drawings, notes, memories and reflections on everything from Albert Camus to global capitalism, Confabulations takes us to what is 'true, essential and urgent'.");
         assertThat(product.price()).isEqualTo(new BigDecimal("25.00"));
-
     }
 
     @Test
-    void shouldReturnNotFoundProductCodeNotExits(){
-        String code ="invalid_product_code";
+    void shouldReturnNotFoundProductCodeNotExits() {
+        String code = "invalid_product_code";
         given().contentType(ContentType.JSON)
                 .when()
-                .get("/api/products/{code}",code)
+                .get("/api/products/{code}", code)
                 .then()
                 .statusCode(404)
                 .body("status", is(404))
                 .body("title", is("Product Not Found"))
                 .body("detail", is("Product with code " + code + " not found"));
-
     }
 }
