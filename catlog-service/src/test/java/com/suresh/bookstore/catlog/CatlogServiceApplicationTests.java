5006e2300558917132156fbca465a8c6bc128dce
Mamidisetty <127787361+Mamidisetty@users.noreply.github.com>
2025-03-22 23:28:11 +0530

    modified test files

--- catlog-service/src/test/java/com/suresh/bookstore/catlog/CatlogServiceApplicationTests.java
+++ catlog-service/src/test/java/com/suresh/bookstore/catlog/CatlogServiceApplicationTests.java
@@ -1,12 +1,8 @@
 package com.suresh.bookstore.catlog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class CatlogServiceApplicationTests {
+class CatlogServiceApplicationTests extends AbstractIT {
 
     @Test
     void contextLoads() {}
