13a84f3837362bd2cc401f5aacfc80a890793e7a
Mamidisetty <127787361+Mamidisetty@users.noreply.github.com>
2025-03-23 22:27:17 +0530

    modified pagedresult page

--- catlog-service/src/main/java/com/suresh/bookstore/catlog/domain/PagedResult.java
+++ catlog-service/src/main/java/com/suresh/bookstore/catlog/domain/PagedResult.java
@@ -3,12 +3,11 @@ package com.suresh.bookstore.catlog.domain;
 import java.util.List;
 
 public record PagedResult<T>(
-
-    List<T> data,
-    Long totalElements,
-    int pageNumber,
-    int totalPages,
-    boolean isFirst,
-    boolean isLast,
-    boolean hasNext,
-    boolean hasPrevious){}
+        List<T> data,
+        Long totalElements,
+        int pageNumber,
+        int totalPages,
+        boolean isFirst,
+        boolean isLast,
+        boolean hasNext,
+        boolean hasPrevious) {}
